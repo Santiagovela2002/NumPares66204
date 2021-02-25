437b6662ba580bb07d7989206146e76f837f75a9
Santiago Vela <Santiago Vela@DESKTOP-KIIP8R7>
2021-02-24 21:10:23 -0600

    Mi primer comit.

---
+++ src/numerospares66204/Numerospares66204.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numerospares66204;
+
+/**
+ *
+ * @author Santiago Vela
+ */
+public class Numerospares66204 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int i = 0;
+        
+        while (i<94)
+        {
+        if(i%2 == 0){
+            System.out.println("i = " + i);
+        }
+        i++;
+        
+        }
+            
+    }
+    
+}
